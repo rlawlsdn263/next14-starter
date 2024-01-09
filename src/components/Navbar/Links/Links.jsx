b5420f6b0d3432125a627d2fb139f2282db69600
rlawlsdn263 <rlawlsdn263@naver.com>
2024-01-09 16:21:09 +0900

    [✨기능] 모바일용 네비게이션 메뉴
    모바일용 메뉴를 별도로 만들고, `open` 상태에 따라 렌더링이 되도록 했다.
    
    또한, 미디어 쿼리로 화면이 768px이 됐을 경우, 화면에 보여지도록 했다.

--- src/components/Navbar/Links/Links.jsx
+++ src/components/Navbar/Links/Links.jsx
@@ -1,29 +1,52 @@
+"use client";
+
+import { useState } from "react";
 import { links } from "@/constants/_index";
 import NavLink from "./NavLink/NavLink";
 import styles from "./Links.module.css";
 
 export default function Links() {
+  // 메뉴 버튼
+  const [open, setOpen] = useState(false);
+
   // 임시 로그인 상태
   const session = true;
   const isAdmin = true;
 
   return (
-    <nav className={styles.links}>
-      {/* 링크 */}
-      {links.map((link) => {
-        return <NavLink key={link.id} item={link} />;
-      })}
-      {/* 세션 */}
-      {session ? (
-        // 어드민 계정일 경우
-        <>
-          {isAdmin && <NavLink item={{ title: "Admin", path: "/admin" }} />}
-          <button className={styles.logout}>Logout</button>
-        </>
-      ) : (
-        // 일반 계정일 경우
-        <NavLink item={{ title: "Login", path: "/login" }} />
-      )}
-    </nav>
+    <>
+      <div className={styles.container}>
+        <nav className={styles.links}>
+          {/* 링크 */}
+          {links.map((link) => (
+            <NavLink key={link.id} item={link} />
+          ))}
+          {/* 세션 */}
+          {session ? (
+            // 어드민 계정일 경우
+            <>
+              {isAdmin && <NavLink item={{ title: "Admin", path: "/admin" }} />}
+              <button className={styles.logout}>Logout</button>
+            </>
+          ) : (
+            // 일반 계정일 경우
+            <NavLink item={{ title: "Login", path: "/login" }} />
+          )}
+        </nav>
+        <button
+          className={styles.menuButton}
+          onClick={() => setOpen((prev) => !prev)}
+        >
+          Menu
+        </button>
+        {open && (
+          <div className={styles.mobileLinks}>
+            {links.map((link) => (
+              <NavLink key={link.id} item={link} />
+            ))}
+          </div>
+        )}
+      </div>
+    </>
   );
 }
