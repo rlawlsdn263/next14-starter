9b7f9fe3c6188fe148d89a0a2fdd3362277e0a03
rlawlsdn263 <rlawlsdn263@naver.com>
2024-01-09 11:57:21 +0900

    [🗼마크업] `Navbar` 컴포넌트 생성

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,3 +1,14 @@
+import Link from "next/link";
+
 export default function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <div>
+      <h1>Logo</h1>
+      <nav>
+        <Link href="/">Home</Link>
+        <Link href="/about">About</Link>
+        <Link href="/contact">Contact</Link>
+      </nav>
+    </div>
+  );
 }
