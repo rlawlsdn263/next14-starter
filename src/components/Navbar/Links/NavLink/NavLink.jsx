69f36912cca92dd713c5dc469d3d0aef68438690
rlawlsdn263 <rlawlsdn263@naver.com>
2024-01-09 14:39:37 +0900

    [✨기능] 액티브링크 기능
    `usePathname` 훅을 사용해 사용자의 현재 경로에 대한 정보를 불러와 `className` 속성에서 `pathName`과 `item.path`와 비교해 조건부로 스타일을 변경해줬다. 이로써 액티브 링크 기능을 완성했다.
    
    참고로, `usePathname`은 현재 경로를 알려주기 때문에 라우터의 이름이 아니라 경로로 비교를 했다.

--- src/components/Navbar/Links/NavLink/NavLink.jsx
+++ src/components/Navbar/Links/NavLink/NavLink.jsx
@@ -1,15 +1,20 @@
+"use client";
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import styles from "./NavLink.module.css";
 
 export default function NavLink({ item }) {
+  const pathName = usePathname();
 
   const { path, title } = item;
 
   return (
     <Link
       href={path}
+      className={`${styles.container} ${
+        pathName === item.path && styles.active
+      }`}
     >
       {title}
     </Link>
