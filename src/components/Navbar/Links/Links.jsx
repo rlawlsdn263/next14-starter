7a3dd79f11796572982fab8d0b433c30b16c7d38
rlawlsdn263 <rlawlsdn263@naver.com>
2024-01-09 14:21:27 +0900

    [💄스타일] `Links` 컴포넌트 기본 스타일 작업

--- src/components/Navbar/Links/Links.jsx
+++ src/components/Navbar/Links/Links.jsx
@@ -1,14 +1,19 @@
 import Link from "next/link";
 import { links } from "@/constants/_index";
+import styles from "./Links.module.css";
 
 export default function Links() {
-  return links.map((link) => {
-    const { id, path, title } = link;
+  return (
+    <nav className={styles.links}>
+      {links.map((link) => {
+        const { id, path, title } = link;
 
-    return (
-      <Link key={id} href={path}>
-        {title}
-      </Link>
-    );
-  });
+        return (
+          <Link key={id} href={path}>
+            {title}
+          </Link>
+        );
+      })}
+    </nav>
+  );
 }
