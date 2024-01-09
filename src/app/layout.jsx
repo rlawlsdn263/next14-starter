7f31c4ab2de7684461bb937306243707645546c4
rlawlsdn263 <rlawlsdn263@naver.com>
2024-01-09 11:17:24 +0900

    [🗼마크업] `layout.jsx`에 `Header`, `Footer` 컴포넌트 추가
    `layout`은 모든 `page`에 공통적으로 사용되는 UI를 다루는 페이지다.
    
    `Header`와 `Footer` 컴포넌트는 모든 `page`에서도 보여야하기에 공용 컴포넌트로 `layout`에 사용이 됐다.

---
+++ src/app/layout.jsx
@@ -0,0 +1,22 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { Navbar, Footer } from "@/components/_index";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Next App",
+  description: "Next.js starter app",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <Navbar />
+        {children}
+        <Footer />
+      </body>
+    </html>
+  );
+}
