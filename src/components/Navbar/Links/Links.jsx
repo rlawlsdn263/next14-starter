6a5729cebe997aea75b91f3b89484aa03d134072
rlawlsdn263 <rlawlsdn263@naver.com>
2024-01-09 14:59:28 +0900

    [✨기능] 세션에 따른 `Links` 확장 기능
    사용자의 등급과 세션 상태에 따라 네비게이션 바에 추가 버튼이 생기도록 했다.
    
    현재는 임시로 기능 구현만 해두기 위해 `Links`에 `session`과 `isAdmin` 변수를 각각 선언하여 사용자 상태를 흉내냈다.
    
    컴포넌트 하단에 `session`이 `true`라면 로그아웃 버튼이 나온다. 이때 사용자가 `admin`이라면 어드민 페이지로 이동할 수 있는 버튼도 추가로 생겨난다.
    
    반대로, `session`이 `false`라면 로그인 페이지로 이동시키기 위한 링크가 등장한다.

--- src/components/Navbar/Links/Links.jsx
+++ src/components/Navbar/Links/Links.jsx
@@ -3,11 +3,27 @@ import NavLink from "./NavLink/NavLink";
 import styles from "./Links.module.css";
 
 export default function Links() {
+  // 임시 로그인 상태
+  const session = true;
+  const isAdmin = true;
+
   return (
     <nav className={styles.links}>
+      {/* 링크 */}
       {links.map((link) => {
         return <NavLink key={link.id} item={link} />;
       })}
+      {/* 세션 */}
+      {session ? (
+        // 어드민 계정일 경우
+        <>
+          {isAdmin && <NavLink item={{ title: "Admin", path: "/admin" }} />}
+          <button className={styles.logout}>Logout</button>
+        </>
+      ) : (
+        // 일반 계정일 경우
+        <NavLink item={{ title: "Login", path: "/login" }} />
+      )}
     </nav>
   );
 }
