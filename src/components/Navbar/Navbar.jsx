ac4fa0e41f750aa83bf93ee462d4ce3a80fd2fdf
rlawlsdn263 <rlawlsdn263@naver.com>
2024-01-10 20:52:48 +0900

    [🗼마크업] `Navbar` 로고 `Link`로 변경

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,13 @@
+import Link from "next/link";
 import Links from "./Links/Links";
 import styles from "./Navbar.module.css";
 
 export default function Navbar() {
   return (
     <header className={styles.container}>
-      <h1 className={styles.logo}>Logo</h1>
+      <h1 className={styles.logo}>
+        <Link href="/">Logo</Link>
+      </h1>
       <Links />
     </header>
   );
