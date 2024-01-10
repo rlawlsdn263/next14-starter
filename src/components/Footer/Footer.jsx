130eb625eb9daa8bb264266d3df7dbd93c7508f8
rlawlsdn263 <rlawlsdn263@naver.com>
2024-01-10 21:18:07 +0900

    [🗼마크업] `Footer` 마크업 완성

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,3 +1,12 @@
+import styles from "./Footer.module.css";
+
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.logo}>JINUONG</div>
+      <div className={styles.text}>
+        JINUONG creative thoughts agency & All rights reserved.
+      </div>
+    </div>
+  );
 }
