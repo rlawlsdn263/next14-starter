8ba29ecafbdc7a1e8d82946ca7743661fb39f847
rlawlsdn263 <rlawlsdn263@naver.com>
2024-01-10 10:26:32 +0900

    [✨기능] Image로 이미지 최적화해서 사용하기

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,3 +1,16 @@
+import Image from "next/image";
+import styles from "./About.module.css";
+
 export default function About() {
-  return <div>About</div>;
+  return (
+    <div>
+      <div className={styles.imgContainer}>
+        <Image
+          src="https://images.pexels.com/photos/16675647/pexels-photo-16675647.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+          alt="about"
+          fill
+        />
+      </div>
+    </div>
+  );
 }
