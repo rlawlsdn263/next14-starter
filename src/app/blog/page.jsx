f177828302fc7d86890d333219a2fb7c5eaf9801
rlawlsdn263 <rlawlsdn263@naver.com>
2024-01-11 15:44:04 +0900

    [🗼마크업] `Blog` 마크업 완성

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,3 +1,21 @@
+import { PostCard } from "@/components/_index";
+import styles from "./Blog.module.css";
+
 export default function Blog() {
-  return <div>Blog</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.post}>
+        <PostCard />
+      </div>
+      <div className={styles.post}>
+        <PostCard />
+      </div>
+      <div className={styles.post}>
+        <PostCard />
+      </div>
+      <div className={styles.post}>
+        <PostCard />
+      </div>
+    </div>
+  );
 }
