56104102f1b8415b14fb50262c3c97556784a52e
rlawlsdn263 <rlawlsdn263@naver.com>
2024-01-16 14:11:06 +0900

    [✨기능] 서버 컴포넌트에서 API 요청 보내기
    `fetch`를 사용해 API 요청을 보내서 데이터를 받아올 수 있다.

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,23 +1,27 @@
 import { PostCard } from "@/components/_index";
 import styles from "./Blog.module.css";
 
-export default function Blog(params, searchParams) {
-  console.log(params);
-  console.log(searchParams);
+/* 데이터 패칭하는 함수 */
+const getData = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+
+  if (!res.ok) {
+    throw new Error("에러남");
+  }
+
+  return res.json();
+};
+
+export default async function Blog() {
+  const posts = await getData();
+
   return (
     <div className={styles.container}>
-      <div className={styles.post}>
-        <PostCard />
-      </div>
-      <div className={styles.post}>
-        <PostCard />
-      </div>
-      <div className={styles.post}>
-        <PostCard />
-      </div>
-      <div className={styles.post}>
-        <PostCard />
-      </div>
+      {posts.map((post) => (
+        <div className={styles.post} key={post.id}>
+          <PostCard post={post} />
+        </div>
+      ))}
     </div>
   );
 }
--- src/components/PostCard/PostCard.jsx
+++ src/components/PostCard/PostCard.jsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 import Image from "next/image";
 import styles from "./PostCard.module.css";
 
-export default function PostCard() {
+export default function PostCard({ post }) {
   return (
     <div className={styles.container}>
       <div className={styles.top}>
@@ -17,13 +17,8 @@ export default function PostCard() {
         <span className={styles.date}>01.01.2024</span>
       </div>
       <div className={styles.bottom}>
-        <h1 className={styles.title}>Title</h1>
-        <p className={styles.desc}>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad possimus
-          odio repudiandae praesentium optio consequatur assumenda nam a
-          molestiae vel quibusdam, explicabo consectetur est magni laudantium
-          omnis provident sed pariatur.
-        </p>
+        <h1 className={styles.title}>{post.title}</h1>
+        <p className={styles.desc}>{post.body}</p>
         <Link href="/blog/post" className={styles.link}>
           READ MORE
         </Link>
