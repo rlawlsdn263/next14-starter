4522bd70832361f920cfb063c09e4e38172d2e5e
rlawlsdn263 <rlawlsdn263@naver.com>
2024-01-16 11:50:46 +0900

    [✨기능] `useRouter` 훅으로 네비게이션 조작하기
    `useRouter`를 사용하면 사용자로 하여금 페이지를 이동, 새로고침, 이전 페이지 혹은 다음 페이지로 넘어가긱 등을 수행할 수 있다.

--- src/app/navigationtest/page.jsx
+++ src/app/navigationtest/page.jsx
@@ -1,11 +1,26 @@
+"use client";
+
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 export default function NavigationTestPage() {
+  const router = useRouter();
+
+  const handleClick = () => {
+    console.log("클릭!");
+    // router.push("/");
+    // router.replace("/"); 브라우저 히스토리스택에 새로운 엔트리 추가X
+    // router.refresh(); 새로고침해서 서버에 새로운 요청을 보내 데이터 갱신해줌
+    // router.back(); 이전 페이지 열기
+    // router.forward(); 다음 페이지 열
+  };
+
   return (
     <div>
       <Link href="/" prefetch={false}>
         Click Here!
       </Link>
+      <button onClick={handleClick}>Write and Redirect</button>
     </div>
   );
 }
