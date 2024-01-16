ecbca44b22a1e0479e4295a02556f55ee8911302
rlawlsdn263 <rlawlsdn263@naver.com>
2024-01-16 11:18:30 +0900

    [🐛버그] `suppressHydrationWarning`으로 경고창 꺼버리기
    `suppressHydrationWarning`을 사용해 하이드레이션 에러 경고를 꺼버리는 방법이다.

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,22 +1,28 @@
+"use client";
+
 import Image from "next/image";
 import styles from "./Contact.module.css";
-import dynamic from "next/dynamic";
+// import dynamic from "next/dynamic";
 // import HydrationTest from "@/components/HydrationTest";
 
 /* 다이나믹 import */
 // 다음처럼 컴포넌트를 import하고 ssr: false를 해주면 SSR 없이 import를 해준다
-const HydrationTestNoSSR = dynamic(() => import("@/components/HydrationTest"), {
-  ssr: false,
-});
+// const HydrationTestNoSSR = dynamic(() => import("@/components/HydrationTest"), {
+//   ssr: false,
+// });
 
 export default function Contact() {
+  const a = Math.random();
+  console.log(a);
+
   return (
     <div className={styles.container}>
       <div className={styles.imgContainer}>
         <Image src="/contact.png" alt="contact" fill className={styles.img} />
       </div>
       <div className={styles.formContainer}>
-        <HydrationTestNoSSR />
+        {/* <HydrationTestNoSSR /> */}
+        <div suppressHydrationWarning>{a}</div>
         <form action="" className={styles.form}>
           <input type="text" placeholder="Name and Surname" />
           <input type="text" placeholder="Email Address" />
