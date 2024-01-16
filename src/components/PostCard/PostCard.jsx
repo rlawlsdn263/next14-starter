b5e6f870dad755481a1c990031f8b8e22b8b0d09
rlawlsdn263 <rlawlsdn263@naver.com>
2024-01-16 14:22:24 +0900

    [✨기능] 동적 라우팅 구현
    `PostCard`의 `Link`의 `href` 속성에 `post.id`를 슬러그값으로 할당해 동적 라우팅을 구현했다.

--- src/components/PostCard/PostCard.jsx
+++ src/components/PostCard/PostCard.jsx
@@ -19,7 +19,7 @@ export default function PostCard({ post }) {
       <div className={styles.bottom}>
         <h1 className={styles.title}>{post.title}</h1>
         <p className={styles.desc}>{post.body}</p>
-        <Link href="/blog/post" className={styles.link}>
+        <Link href={`/blog/${post.id}`} className={styles.link}>
           READ MORE
         </Link>
       </div>
