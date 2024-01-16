842fd6027533c5e50d8ba52e5f48f48632c7dad9
rlawlsdn263 <rlawlsdn263@naver.com>
2024-01-16 11:06:19 +0900

    [🐛버그] `useEffect`로 하이드레이션 에러 해결
    넥스트는 서버 -> 클라이언트 순서로 컴포넌트를 확인한다. 그렇기 때문에 서버와 클라이언트가 일치하지 않는 값을 렌더링할 경우, 하이드레이션 에러가 발생한다.
    
    이를 해결하기 위한 한 가지 방법으로는 클라이언트 컴포넌트에서 `useEffect`로 렌더링이 클라이언트에서만 발생하도록 처리하는 것이다.
    
    `a`는 `Math.random()`으로 서버와 클라이언트에서 각각 다른 값을 출력하고 있다. 그래서 `a`를 렌더링할 경우, 하이드레이션 에러가 발생했다.
    
    하지만, `useEffect`로 `isClient`의 상태를 조절하고, 이 값에 따라 `a`를 렌더링한다면 `a`는 클라이언트에서만 렌더링되기 때문에 값이 서버와 불일치하더라도 하이드레이션 에러가 발생하지 않는다.

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,13 +1,26 @@
+"use client";
+
 import Image from "next/image";
 import styles from "./Contact.module.css";
+import { useEffect, useState } from "react";
 
 export default function Contact() {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  const a = Math.random();
+  console.log(a);
+
   return (
     <div className={styles.container}>
       <div className={styles.imgContainer}>
         <Image src="/contact.png" alt="contact" fill className={styles.img} />
       </div>
       <div className={styles.formContainer}>
+        {isClient && a}
         <form action="" className={styles.form}>
           <input type="text" placeholder="Name and Surname" />
           <input type="text" placeholder="Email Address" />
@@ -19,7 +32,7 @@ export default function Contact() {
             rows="10"
             placeholder="Message"
           ></textarea>
-          <button>Send</button>
+          <button onClick={() => console.log("클릭함!")}>Send</button>
         </form>
       </div>
     </div>
